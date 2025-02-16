015c5f1fffcaa024d62248b9f6d42f54f03dc71a
Sagar Shetty <sagarshetty.lm10judo@gmail.com>
2025-02-16 19:30:38 +0530

    users added

--- CommUnity/Login/models.py
+++ CommUnity/Login/models.py
@@ -14,6 +14,7 @@ class UserProfile(models.Model):
     id = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
     full_name = models.CharField(max_length=255)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg',null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- CommUnity/faculty/views.py
+++ CommUnity/faculty/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+from faculty.models import Faculty
 # Create your views here.
 
 def faculty_view(request):
     return render(request, 'faculty.html')
 def profile_view(request):
-    return render(request, 'profile.html')
+
+    data = Faculty.objects.filter(id=request.user.id)
+
+    return render(request, 'profile.html', {'data': data})
