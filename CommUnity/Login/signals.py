63ca3b64ed75826288098e4e8234c68398791afc
kushal-s0 <kushal.soni@somaiya.edu>
2025-03-13 20:08:05 +0530

    login fixed

--- CommUnity/Login/signals.py
+++ CommUnity/Login/signals.py
@@ -1,12 +1,29 @@
 # Faculty/signals.py
 
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from .models import Faculty
+# from Login.models import UserProfile
+
+# @receiver(post_save, sender=Faculty)
+# def set_ssv_id(sender, instance, created, **kwargs):
+#     if created and instance.id and not instance.id.ssv_id:
+#         instance.id.ssv_id = instance.id.pk  
+#         instance.id.save()
+
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Faculty
-from Login.models import UserProfile
+from django.contrib.auth import get_user_model
+from .models import UserProfile
 
-@receiver(post_save, sender=Faculty)
-def set_ssv_id(sender, instance, created, **kwargs):
-    if created and instance.id and not instance.id.ssv_id:
-        instance.id.ssv_id = instance.id.pk  
-        instance.id.save()
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(id=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
